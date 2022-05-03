605341d902793732d918cebebb7963f0993bd6f0
mehak2299 <kaurkavneet72@gmail.com>
2022-05-03 17:17:12 +0530

    Add changes

---
+++ JavaScript/Basics/basicsCover/one.js
@@ -0,0 +1,24 @@
+console.time("your code took");
+let myvakr=34;
+console.log(myvakr,typeof myvakr);
+let person={
+  name:"Kav",
+  eyecolor:"brown"
+};
+console.table(person);
+console.timeEnd("your code took");
+
+console.log("Convert number intor string",String(myvakr));
+console.log(typeof Date());
+let d=new Date();
+console.log(typeof d);
+let arr=[1,2,4];
+console.log(typeof (String(arr)));
+let val=18;
+console.log("Convert nos into String"+val.toString());
+let str="124";
+console.log(str,typeof(str));
+console.log("convert");
+console.log(Number(str));
+let nos=parseInt("23.80");
+console.log(typeof nos);
